46de6cc28c5e98de0f6c11f76a5857b2dbe7319c
SanjithBC <111051325+SanjithBC@users.noreply.github.com>
2023-05-24 18:23:07 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     song1 = loadSound("music.mp3");
@@ -7,11 +11,31 @@ function preload() {
 }
 
 function setup() {
-    canvas = createCanvas(600, 500);
+    canvas = createCanvas(500, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized!');
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 
 function draw() {
@@ -20,4 +44,6 @@ function draw() {
 
 function play() {
     song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
 }
